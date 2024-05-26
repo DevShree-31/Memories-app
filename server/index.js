ae4a066f69a6f2c9f18d7590116efb1818ce1598
Shreyansh <ui21cs56@iiitsurat.ac.in>
2024-05-26 11:01:57 +0530

    Initialized a project

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import router from './routes/posts.js'
+const app=express()
+app.use('/posts',router)
+app.use(bodyParser.json({limit:"30mb",extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
+app.use(cors())
+//will use cloud mongoDB 
+const CONNECTION_URL="mongodb+srv://shreyansh:Shreyansh$123@cluster0.ap0tnyi.mongodb.net/"
+const PORT=process.env.PORT ||5000;
+mongoose.connect(CONNECTION_URL).then(
+    ()=>app.listen(PORT,()=>console.log(`Connection established at port ${PORT}`)))
+    .catch((error)=>console.log(error.message));
